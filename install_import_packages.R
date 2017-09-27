458d83bb42d19fa64d1ba3936841026ed4de276a
mfaraki <masoud.faraki@data61.csiro.au>
2017-09-27 12:47:30 +1000

    install or import required packages

---
+++ install_import_packages.R
@@ -0,0 +1,11 @@
+install_import_packages <- function(required_packages){
+  all_packages <- installed.packages()
+  all_packages <- all_packages[,"Package"]
+  
+  #install -> import the requirements
+  for(i in length(required_packages)) 
+    if(!is.element(required_packages[i],  all_packages))
+      install.packages(required_packages[i])
+  dummy <- lapply(required_packages,library,character.only=TRUE,quietly = TRUE)
+}
+  
