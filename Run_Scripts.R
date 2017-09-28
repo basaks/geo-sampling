a5db2829f6028f20b92a9e9f0496892c17b12d9a
mfaraki <masoud.faraki@data61.csiro.au>
2017-09-28 15:46:44 +1000

    run script

---
+++ Run_Scripts.R
@@ -0,0 +1,10 @@
+covariate_names <- c("rat15thk","tot15","modis4_te","si_geol1") # si_geol1 is categorical
+sirsam_data = "/home/masoud/GA_data/GA-cover2"; # Where your input data exist
+shapefile_name = "geochem_sites" # Traget points
+exp_folder = "."   # Output folder
+no_samples = 50 # Number of output samples. Try different values or for example use seq(20,50,10) to have 20 30 40 50 sampled points
+
+
+source("sample_clhs.R")
+sample_clhs(covariate_names, sirsam_data, shapefile_name, exp_folder, no_samples)
+  
