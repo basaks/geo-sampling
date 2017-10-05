2dcb2c5181f9bd9e5976fb5e7a3109aa5dd80691
MF <masoud.faraki@nicta.com.au>
2017-10-05 14:00:27 +1100

    Update install_import_packages.R

--- install_import_packages.R
+++ install_import_packages.R
@@ -5,7 +5,7 @@ install_import_packages <- function(required_packages){
   #install -> import the requirements
   for(i in length(required_packages)) 
     if(!is.element(required_packages[i],  all_packages))
-      install.packages(required_packages[i])
+      install.packages(required_packages[i],repos="https://mirror.aarnet.edu.au/pub/CRAN/")
   dummy <- lapply(required_packages,library,character.only=TRUE,quietly = TRUE, verbose = FALSE)
 }
   
