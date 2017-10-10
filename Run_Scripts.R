8d1a855f8be2a2a8b19881f7f47da8c0e00b2f33
mfaraki <masoud.faraki@data61.csiro.au>
2017-10-10 17:10:35 +1100

    Inputs changes

--- Run_Scripts.R
+++ Run_Scripts.R
@@ -1,13 +1,12 @@
-covariate_names = c("rat15thk","tot15","modis4_te","si_geol1") # si_geol1 is categorical
-#sirsam_data = "/home/masoud/GA_data/GA-cover2"; # Where your input data exist
-#sirsam_data = "/g/data/ge3/feizhang/GA-cover2-LHC-Test"; # Where your input data exist
-sirsam_data = "/short/ge3/jrw547/GA-cover2"
+covariate_file = "sirsam_covariates_Cr.txt"#
+sirsam_data = "/home/masoud/GA_data/GA-cover2"; # Where your input data exist
+#sirsam_data = "/short/ge3/jrw547/GA-cover2"
 shapefile_name = "geochem_sites" # Traget points
 exp_folder = "."   # Output folder
 no_samples = 50 # Number of output samples. Try different values or for example use seq(20,50,10) to have 20 30 40 50 sampled points
 
 source("sample_clhs.R")
-sample_clhs(covariate_names, sirsam_data, shapefile_name, exp_folder, no_samples)
+sample_clhs(covariate_file, sirsam_data, shapefile_name, exp_folder, no_samples)
 
 #width = 0.005 # Buffer size
 #road_shapefile_name = "Roads_Sir_Sam"  # Line segments
