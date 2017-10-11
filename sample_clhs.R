db59e391a4179131321dc37c2a8ace287e5e2237
mfaraki <masoud.faraki@data61.csiro.au>
2017-10-11 14:55:00 +1100

    Random sampling added.

--- sample_clhs.R
+++ sample_clhs.R
@@ -24,19 +24,20 @@ sample_clhs <- function(covariate_file, data_folder, shape_file, output_folder, 
   
   for (i in no_samples){  
     #par(mfrow = c(3,1))
-    dsn_folder_cLHS = paste(output_folder, paste0("cLHS_", i,"points"), sep = "/")
-    dir.create(dsn_folder_cLHS, recursive = TRUE)
-    print(paste("Running cLHS to sample", as.character(i),"points. Check", dsn_folder_cLHS,"for results when finished."))
-    res <- clhs(df, size = i, iter = 10000, progress = TRUE, simple = FALSE , weights = list(numeric = 1, factor = 1, correlation =1))
-    
-    S_clhs <- S[res$index_samples,]
+    #dsn_folder_cLHS = paste(output_folder, paste0("cLHS_", i,"points"), sep = "/")
+    #dir.create(dsn_folder_cLHS, recursive = TRUE)
+    #print(paste("Running cLHS to sample", as.character(i),"points. Check", dsn_folder_cLHS,"for results when finished."))
+    #res <- clhs(df, size = i, iter = 10000, progress = TRUE, simple = FALSE , weights = list(numeric = 1, factor = 1, correlation =1))
+    #S_clhs <- S[res$index_samples,]
     #plot(coordinates(S_clhs))
-    writeOGR(S_clhs, dsn = dsn_folder_cLHS, layer = paste(shape_file, as.character(i), "cLHS", sep = "_"), driver = "ESRI Shapefile", overwrite_layer = TRUE)
+    #writeOGR(S_clhs, dsn = dsn_folder_cLHS, layer = paste(shape_file, as.character(i), "cLHS", sep = "_"), driver = "ESRI Shapefile", overwrite_layer = TRUE)
     
-    #  dsn_folder_RND = paste(output_folder, paste0("RND_", i,"points"), sep = "/")
-    #  RND_samples <- sample(1:nrow(S),i)
-    #  S_RND <- S[RND_samples,]
-    #  writeOGR(S_RND, dsn = dsn_folder_RND, layer = shape_file, driver = "ESRI Shapefile", overwrite_layer = TRUE)
+    dsn_folder_RND = paste(output_folder, paste0("RND_", i,"points"), sep = "/")
+    dir.create(dsn_folder_RND, recursive = TRUE)
+    print(paste("Randomly sampling", as.character(i),"points. Check", dsn_folder_RND,"for results."))
+    RND_samples <- sample(1:nrow(S),i)
+    S_RND <- S[RND_samples,]
+    writeOGR(S_RND, dsn = dsn_folder_RND, layer = paste(shape_file, as.character(i), "RND", sep = "_"), driver = "ESRI Shapefile", overwrite_layer = TRUE)
     #plot(coordinates(ss), add=TRUE )
     
     #Save histograms in the folders
@@ -56,9 +57,9 @@ sample_clhs <- function(covariate_file, data_folder, shape_file, output_folder, 
     #    dev.off()
     # }
   }
-  par(mfrow = c(1,1))
-  pdf("Objective Function.pdf")
-  plot(res)
-  dev.off()
+#  par(mfrow = c(1,1))
+#  pdf("Objective Function.pdf")
+#  plot(res)
+#  dev.off()
 }
 #dev.print(pdf, paste(dsn_folder_cLHS, "cost.pdf", sep = "/"))
