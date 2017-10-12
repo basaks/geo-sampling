a31b7525d6ed9c73707060710f62228fb2870061
mfaraki <masoud.faraki@data61.csiro.au>
2017-10-12 15:36:32 +1100

    CRS conversion added.

--- Read_Covariates.R
+++ Read_Covariates.R
@@ -3,13 +3,11 @@
 Read_Covariates <- function(cov_file, data_folder) {
   all_cov <- read.table(paste(data_folder,cov_file, sep = "/"), header = FALSE)
   print("Reading input covariates.")
+  expected_CRS <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+  print(paste("Expected CRS is", expected_CRS))
   num_categorical <- 0
-  fname <- trimws(as.character(all_cov$V1[1]))
-  print(fname)
-  r <- raster(fname)
-  cov_stack <- stack(r)
   
-  for(i in 2:length(all_cov$V1)){
+  for(i in 1:length(all_cov$V1)){
     fname <- trimws(as.character(all_cov$V1[i]))
     print(fname)
     if(!startsWith(fname , "#")){
@@ -23,7 +21,16 @@ Read_Covariates <- function(cov_file, data_folder) {
         r[] <- as.factor(r[])
         r@data@isfactor
       }
-      cov_stack <- stack(cov_stack, r)
+      #CRS check
+      if(!compareCRS(expected_CRS,r@crs)){
+        print(paste("CRS converted for",fname))
+        r@crs <- expected_CRS
+      }
+      #Stack 
+      if(i == 1)
+        cov_stack <- stack(r)
+      else
+        cov_stack <- stack(cov_stack, r)
     }
   }
   print(paste(as.character(length(cov_stack@layers)) , "covariates were read out of which", as.character(num_categorical), "recognized as categorical covariates."))
