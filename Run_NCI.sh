750c79d955b7642f5cb9b3a6ac3aff695a192082
mfaraki <masoud.faraki@data61.csiro.au>
2017-10-16 13:48:12 +1100

    NCI Support.

---
+++ Run_NCI.sh
@@ -0,0 +1,11 @@
+module unload intel-fc intel-cc
+module load intel-fc/16.0.3.210
+module load intel-cc/16.0.3.210
+module load gdal/2.1.3
+module load  proj/4.8.0
+module load geos/3.5.0
+
+module load  R/3.4.0
+R --vanilla  output
+
+
