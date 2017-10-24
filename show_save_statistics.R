bbca35f886926a2c878a70673acf25f48d03b5d5
mfaraki <masoud.faraki@data61.csiro.au>
2017-10-24 14:54:49 +1100

    Statistics corrected.

--- show_save_statistics.R
+++ show_save_statistics.R
@@ -6,14 +6,20 @@ show_save_statistics <- function(cov_names, res, RND_samples, dsn_folder){
     pdf(paste(dsn_folder, paste0(cov_names[i],".pdf"), sep = "/"))
     
     tmp_cov <- res$initial_object[[i]]
-    if(!is.numeric(tmp_cov))  tmp_cov <- as.numeric(tmp_cov)
-    hist(tmp_cov, main = paste("Original histogram of",cov_names[i]), xlab = paste("Mean:",mean(tmp_cov), ", Variance", var(tmp_cov)), freq = FALSE)
-    tmp_cov <- res$sampled_data[[i]]
-    if(!is.numeric(tmp_cov))  tmp_cov <- as.numeric(tmp_cov)
-    hist(tmp_cov, main = paste("cLHS histogram of",cov_names[i]), xlab = paste("Mean:",mean(tmp_cov), ", Variance", var(tmp_cov)), freq = FALSE)
+    if(!is.numeric(tmp_cov))  
+      tmp_cov <- as.numeric(tmp_cov)
+    hist(tmp_cov, main = paste("Original histogram of",cov_names[i]), xlab = paste("Mean:",mean(tmp_cov, na.rm = TRUE), ", Variance", var(tmp_cov, na.rm = TRUE)), freq = FALSE)
+    x_lim = range(tmp_cov, na.rm = TRUE)
+
+        tmp_cov <- res$sampled_data[[i]]
+    if(!is.numeric(tmp_cov))  
+      tmp_cov <- as.numeric(tmp_cov)
+    hist(tmp_cov, main = paste("cLHS histogram of",cov_names[i]), xlim = x_lim, xlab = paste("Mean:",mean(tmp_cov, na.rm = TRUE), ", Variance", var(tmp_cov, na.rm = TRUE)), freq = FALSE)
+    
     tmp_cov <- res$initial_object[[i]][RND_samples]
-    if(!is.numeric(tmp_cov))  tmp_cov <- as.numeric(tmp_cov)
-    hist(tmp_cov, main = paste("RND histogram of",cov_names[i]), xlab = paste("Mean:",mean(tmp_cov), ", Variance", var(tmp_cov)), freq = FALSE)
+    if(!is.numeric(tmp_cov))  
+      tmp_cov <- as.numeric(tmp_cov)
+    hist(tmp_cov, main = paste("RND histogram of",cov_names[i]), xlim = x_lim, xlab = paste("Mean:",mean(tmp_cov, na.rm = TRUE), ", Variance", var(tmp_cov, na.rm = TRUE)), freq = FALSE)
     #dev.print(pdf, paste(dsn_folder, paste0(cov_names[i],".pdf"), sep = "/"))
     
     dev.off()
