c80f464d0c7f11f6074f7ac52db81926471ae708
mfaraki <masoud.faraki@data61.csiro.au>
2017-10-24 14:53:16 +1100

    More efficient extraction added. Statistics corrected.

--- Buffer_Sample.R
+++ Buffer_Sample.R
@@ -1,23 +1,21 @@
 # Buffer along Roads and then sample representatively
 ############################################################
-Buffer_Sample <- function(covariate_file, data_folder, shape_file, output_folder, no_samples, width){
-
-source("show_save_statistics.R")
+Buffer_Sample <- function(covariate_list, data_folder, shape_file, output_folder, no_samples, width){
+  
 source("install_import_packages.R")
+source("show_save_statistics.R")
 source("Read_Covariates.R")
   
 required_packages <- c("raster","clhs","rgdal","moments","rgeos")
 dummy <- install_import_packages(required_packages)
 
 #Read only one covaraite file for some statistics
-all_cov_files <- Parse_Text(covariate_file)
-r <- raster(all_cov[1,])
+all_cov_files <- Parse_Text(covariate_list)
+r <- raster(all_cov_files[1,])
 
 road_shapefile <- readOGR(dsn = data_folder, layer = shape_file, verbose = FALSE)
 road_buff <- gBuffer(road_shapefile, byid = TRUE, width = width)
-pdf("Buffered_Roads.pdf")
-plot(road_buff)
-dev.off()  
+
 #dev.print(pdf, paste(output_folder,"Buffered_Roads.pdf",sep = "/"))
 
 road_filename <- paste(output_folder,paste0("roads_Buffer_",as.character(width)),sep = "/")
@@ -36,16 +34,10 @@ for(i in 2:length(road_extracted)) df <- rbind(df , data.frame(road_extracted[i]
 locations <- df[,1]
 print(paste("Total number of pixels is", as.character(r@ncols * r@nrows), ",", length(locations), "pixels extracted along the roads.", sep = " "))
 
+df <- Read_Covariates(covariate_list, locations)
 
-df <- Read_Covariates(covariate_file, locations)
-
-# cov_intersected <- extract(cov_stack,locations)
-# df <- data.frame(cov_intersected)
-# 
-# #Convert to categorical
-# for(i in 1:length(cov_stack@layers)) 
-#   if(cov_stack@layers[[i]]@data@isfactor) 
-#     df[,i] <- as.factor(df[,i])
+r_coords <- coordinates(r)
+r_coords <- r_coords[locations,]
 
 for (i in no_samples){  
   dsn_folder_cLHS = paste(output_folder, paste0("cLHS_", i,"points"), sep = "/")
@@ -54,10 +46,15 @@ for (i in no_samples){
   res <- clhs(df, size = i, iter = 10000, progress = TRUE, simple = FALSE , weights = list(numeric = 1, factor = 1, correlation =1))
   
   RND_samples <- sample(1:length(locations),i)
-  dummy <- show_save_statistics(cov_names, res, RND_samples, dsn_folder_cLHS)
+  dummy <- show_save_statistics(colnames(df), res, RND_samples, dsn_folder_cLHS)
+  
+  pdf(paste(dsn_folder_cLHS,"Objective_Function.pdf", sep = '/'))
+  plot(res)
+  dev.off()
+  
+  pdf(paste(dsn_folder_cLHS,"Buffered_Roads.pdf", sep = '/'))
+  plot(road_buff)
+  points(r_coords[res$index_samples,], col = 'red')
+  dev.off()
 }
-
-pdf("Objective Function.pdf")
-plot(res)
-dev.off()
 }
