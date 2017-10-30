a796006867d0d5b36167133f0ed15b04f0c3a578
mfaraki <masoud.faraki@data61.csiro.au>
2017-10-30 14:39:50 +1100

    All scenarios plus balanced sampling.

--- Run_Scripts.R
+++ Run_Scripts.R
@@ -1,4 +1,4 @@
-covariate_file = "/home/masoud/GA_data/GA-cover2/sirsam_covariates_Na.txt" #
+covariate_file = "/home/masoud/GA_data/GA-cover2/sirsam_covariates_Cr.txt" #
 sirsam_data = "/home/masoud/GA_data/GA-cover2"; # Where your input data exist
 #sirsam_data = "/short/ge3/jrw547/GA-cover2"
 exp_folder = "."   # Output folder
@@ -16,12 +16,19 @@ Buffer_Sample(covariate_file, sirsam_data, road_shapefile_name, exp_folder, no_s
 shapefile_name = "geochem_sites" # geochem_sites_log Traget points
 sample_clhs(covariate_file, sirsam_data, shapefile_name, exp_folder, no_samples)
 
+#Third scenario: Similar to the first scenario but with an existing model used for weighting the inputs
+width = 0.005 # Buffer size
+existing_model = "/home/masoud/Intenship/LHS/geo-sampling/sirsam_Na_original_prediction.tif"
+road_shapefile_name = "Roads_Sir_Sam"  # Line segments
+Buffer_Sample(covariate_file, sirsam_data, road_shapefile_name, exp_folder, no_samples, width, existing_model)
 
-#existing_model = "/home/masoud/Intenship/LHS/geo-sampling/sirsam_Na_original_prediction.tif"
-#source("sample_clhs_existing_model.R")
-#sample_clhs_existing_model(covariate_file, sirsam_data, shapefile_name, exp_folder, no_samples, existing_model)
+#Fourth scenario: Similar to the second scenario but with an existing model used for weighting the inputs
+existing_model = "/home/masoud/Intenship/LHS/geo-sampling/sirsam_Na_original_prediction.tif"
+shapefile_name = "geochem_sites" # geochem_sites_log Traget points
+source("sample_clhs_existing_model.R")
+sample_clhs(covariate_file, sirsam_data, shapefile_name, exp_folder, no_samples, existing_model)
 
-#source("sample_balanced.R")
-#sample_balanced(covariate_file, sirsam_data, shapefile_name, exp_folder, no_samples)
-
-
+#Balanced sampling. All covariates must be continues valued, no categorical
+source("sample_balanced.R")
+shapefile_name = "geochem_sites" # geochem_sites_log Traget points
+sample_balanced(covariate_file, sirsam_data, shapefile_name, exp_folder, no_samples)
