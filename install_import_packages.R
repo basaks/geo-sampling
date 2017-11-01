42edd415958b700bd1bc9372797de935c1bad21e
mfaraki <masoud.faraki@data61.csiro.au>
2017-11-01 16:58:50 +1100

    A function for installing required packages automatically.

--- install_import_packages.R
+++ install_import_packages.R
@@ -3,7 +3,7 @@ install_import_packages <- function(required_packages){
   all_packages <- all_packages[,"Package"]
   
   #install -> import the requirements
-  for(i in length(required_packages)) 
+  for(i in 1:length(required_packages)) 
     if(!is.element(required_packages[i],  all_packages))
       install.packages(required_packages[i],repos="https://mirror.aarnet.edu.au/pub/CRAN/")
   dummy <- lapply(required_packages,library,character.only=TRUE,quietly = TRUE, verbose = FALSE)
