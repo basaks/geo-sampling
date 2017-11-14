2832f935c06740d3881e15309853cb0160fb8403
mfaraki <masoud.faraki@data61.csiro.au>
2017-11-14 14:16:02 +1100

    Statistics range corrected.

--- show_save_statistics.R
+++ show_save_statistics.R
@@ -1,6 +1,6 @@
-show_save_statistics <- function(cov_names, res, RND_samples, dsn_folder){
+show_save_statistics <- function(cov_names, res, dsn_folder){
  
-  par(mfrow = c(3,1))
+  par(mfrow = c(2,1))
   #Save histograms in the folders
   for(i in 1:length(cov_names)){
     pdf(paste(dsn_folder, paste0(cov_names[i],".pdf"), sep = "/"))
@@ -11,17 +11,12 @@ show_save_statistics <- function(cov_names, res, RND_samples, dsn_folder){
     hist(tmp_cov, main = paste("Original histogram of",cov_names[i]), xlab = paste("Mean:",mean(tmp_cov, na.rm = TRUE), ", Variance", var(tmp_cov, na.rm = TRUE)), freq = FALSE)
     x_lim = range(tmp_cov, na.rm = TRUE)
 
-        tmp_cov <- res$sampled_data[[i]]
+    tmp_cov <- res$sampled_data[[i]]
     if(!is.numeric(tmp_cov))  
       tmp_cov <- as.numeric(tmp_cov)
     hist(tmp_cov, main = paste("cLHS histogram of",cov_names[i]), xlim = x_lim, xlab = paste("Mean:",mean(tmp_cov, na.rm = TRUE), ", Variance", var(tmp_cov, na.rm = TRUE)), freq = FALSE)
     
-    tmp_cov <- res$initial_object[[i]][RND_samples]
-    if(!is.numeric(tmp_cov))  
-      tmp_cov <- as.numeric(tmp_cov)
-    hist(tmp_cov, main = paste("RND histogram of",cov_names[i]), xlim = x_lim, xlab = paste("Mean:",mean(tmp_cov, na.rm = TRUE), ", Variance", var(tmp_cov, na.rm = TRUE)), freq = FALSE)
     #dev.print(pdf, paste(dsn_folder, paste0(cov_names[i],".pdf"), sep = "/"))
-    
     dev.off()
   }
   par(mfrow = c(1,1))
