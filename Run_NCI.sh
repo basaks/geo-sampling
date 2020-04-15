83f8206807114ca9a6c42c0afaf2c0fd905f2f52
Brenainn Moushall <bm5478@gadi-login-01.gadi.nci.org.au>
2020-04-15 10:46:47 +1000

    Add install script for gadi + update requried NCI modules

--- Run_NCI.sh
+++ Run_NCI.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 #PBS -P ge3
-#PBS -q expressbw
-#PBS -l walltime=02:00:00,mem=256GB,ncpus=4,jobfs=100GB
+#PBS -q normal
+#PBS -l walltime=02:00:00,mem=190GB,ncpus=1jobfs=100GB
+#PBS -l storage=gdata/ge3
 #PBS -l wd
 #PBS -j oe
 
-module unload intel-fc intel-cc
-module load intel-fc/16.0.3.210
-module load intel-cc/16.0.3.210
-module load gdal/2.1.3
-module load proj/4.8.0
-module load geos/3.5.0
-module load R/3.4.0
+module load R/3.6.1
+module load gdal/3.0.2
+module load proj/6.2.1
+
+# Set 'R_LIBS' to the directory containing R packages from installation.
+export R_LIBS=
 
 Rscript Run_Scripts.R
-#R --vanilla  output
-
-
---
+++ gadi_install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+module purge
+
+module load R/3.6.1
+module load gdal/3.0.2
+module load proj/6.2.1
+module load intel-compiler
+
+exit_on_failure() {
+    if [[ $1 != 0 ]]
+    then
+        echo $2
+        exit 1
+    fi
+}
+
+if [[ -z ${R_LIBS+x} ]]
+then
+    echo "Installing R packages to `R --slave -e '.libPaths()[1]'`. If this is not correct, change the R_LIBS environment variable."
+else
+    echo "Installing R packages to '$R_LIBS'. If this is not correct, change the R_LIBS environment variable."
+fi
+
+read -p "Continue? (y|Y)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  R --slave -e "install.packages(c('rgdal', 'raster', 'moments', 'rgeos', 'BalancedSampling', 'ggplot2', 'reshape2', 'plyr', 'scales'), repos='https://mirror.aarnet.edu.au/pub/CRAN/')"
+  exit_on_failure $? "Error: could not install R packages."
+  CURDIR=`pwd`
+  cd $HOME
+  # 'clhs' is not available from CRAN at time of writing (14-04-2020), so need to download from archive and install.
+  curl -O https://cran.r-project.org/src/contrib/Archive/clhs/clhs_0.7-2.tar.gz
+  tar -xzf clhs_0.7-2.tar.gz
+  R --slave -e "install.packages('$HOME/clhs', repos=NULL, type='source')"
+  RET=$?
+  rm clhs_0.7-2.tar.gz
+  rm -r clhs
+  cd $CURDIR
+  exit_on_failure $RET "Error: could not install 'clhs'."
+  
+  echo "'geo-sampling' install complete. Ensure you set 'R_LIBS' environment variable to the location where you installed these R packages when submitting a job."
+fi
