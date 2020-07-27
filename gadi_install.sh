b149077194c1a55e2d7270bc426482500d52990c
Bren Moushall <brenainn.moushall@ga.gov.au>
2020-07-27 16:34:36 +1000

    Force install directory
    R_LIBS isn't guaranteed to be the first entry of .libPaths(), which is what package
    installation will default to if 'libs' isn't set.

--- gadi_install.sh
+++ gadi_install.sh
@@ -26,14 +26,14 @@ read -p "Continue? (y|Y)" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  R --slave -e "install.packages(c('rgdal', 'raster', 'moments', 'rgeos', 'BalancedSampling', 'ggplot2', 'reshape2', 'plyr', 'scales'), repos='https://mirror.aarnet.edu.au/pub/CRAN/')"
+  R --slave -e "install.packages(c('rgdal', 'raster', 'moments', 'rgeos', 'BalancedSampling', 'ggplot2', 'reshape2', 'plyr', 'scales'), repos='https://mirror.aarnet.edu.au/pub/CRAN/', lib='$R_LIBS')"
   exit_on_failure $? "Error: could not install R packages."
   CURDIR=`pwd`
   cd $HOME
   # 'clhs' is not available from CRAN at time of writing (14-04-2020), so need to download from archive and install.
   curl -O https://cran.r-project.org/src/contrib/Archive/clhs/clhs_0.7-2.tar.gz
   tar -xzf clhs_0.7-2.tar.gz
-  R --slave -e "install.packages('$HOME/clhs', repos=NULL, type='source')"
+  R --slave -e "install.packages('$HOME/clhs', repos=NULL, type='source', lib='$R_LIBS')"
   RET=$?
   rm clhs_0.7-2.tar.gz
   rm -r clhs
